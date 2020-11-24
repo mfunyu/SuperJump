e24d37534176d1012cdb779ff566a5e21ed2d49e
looc-github <54154940+looc-github@users.noreply.github.com>
2020-11-24 20:41:56 +0900

    Add files via upload

---
+++ SuperJump/Creature.py
@@ -0,0 +1,12 @@
+class Creature():
+    def __init__(self):
+        self.life = 0
+        self.x_position = 0
+        self.y_position = 0
+        self.radius = CREATURERADIUS
+        self.gravity = 0
+        
+    def update(self):
+        
+    def display(self):
+        
