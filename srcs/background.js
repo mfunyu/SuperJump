f5d65f95de51118222fba3f3df00bfa50ab08863
Yuu Funyu <yuu.funyu@bee2link.fr>
2024-12-05 14:53:53 +0100

    feat(background): images to static variables, function

--- srcs/background.js
+++ srcs/background.js
@@ -1,36 +1,39 @@
-const GAME_SPEED = 3
-
 class Background {
-  constructor(background_images) {
-    this.coord_y_first = 0; // y position
-    this.coord_y_second = 0; // y position
-    this.speed = GAME_SPEED;
+  static GAME_SPEED = 3;
+  static NUM_BG_IMGS = 5;
+  static images = [];
 
+  static preload() {
+    for (let i = 0; i < this.NUM_BG_IMGS; i++) {
+      this.images.push(loadImage(IMG_PATH + `bg${i}.png`));
+    }
+  }
 
-    this.images = background_images;
-
+  constructor() {
     this.altitude = 0;
     this.image_index = 0;
-    this.total_img_height = this.images[this.image_index].height;
+    this.total_img_height = Background.images[this.image_index].height;
+
+    this.speed = Background.GAME_SPEED;
   }
 
   display() {
     while (this.total_img_height < this.altitude) {
       this.image_index++;
-      this.total_img_height += this.images[this.image_index].height;
+      this.total_img_height += Background.images[this.image_index].height;
     }
 
-    let visible_img_size = 0;
-    for (let i = 0; windowHeight > visible_img_size; i++) {
-      let image_index = Math.min(this.image_index + i, this.images.length - 1);
-      let current_img = this.images[image_index];
+    let visible_img_height = 0;
+    for (let i = 0; windowHeight > visible_img_height; i++) {
+      let image_index = Math.min(this.image_index + i, Background.NUM_BG_IMGS);
+      let current_img = Background.images[image_index];
 
       if (i == 0)
-        visible_img_size = this.total_img_height - this.altitude;
+        visible_img_height = this.total_img_height - this.altitude;
       else
-        visible_img_size += current_img.height;
+        visible_img_height += current_img.height;
 
-      let y_coordinate = windowHeight - visible_img_size
+      let y_coordinate = windowHeight - visible_img_height
       image(current_img, 0, y_coordinate, windowWidth, current_img.height);
     }
     this.scroll();
